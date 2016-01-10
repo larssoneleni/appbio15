9d54036d06caa8a723a4385df7db981a6a7dba71
Elini Larsson <elinila@kth.se>
2016-01-10 13:19:42 +0100

    code

---
+++ project/results/2015-12-18/code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#coding: utf8
+#####
+# We can run muscle on every file except the tree-files to get a better order??? 
+#(use seaview to get a picture of how the sequences (or the alignments)
+# looks like where we can se how the columns looks.
+# A starting point for the program is where our input is the files 
+# from our data directory (except the tree-files, challange!) and 
+#run muscle to get outputfiles.
+# Next things is to check each column (from our outputfile) with our 3 criterias!
+####
